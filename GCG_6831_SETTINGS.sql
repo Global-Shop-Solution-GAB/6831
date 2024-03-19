2d4b264674b14c956451711b78148365bf346253
Service Web <sw@gssmail.com>
2024-03-19 13:58:30 -0500

    Commit changes in C:\Websites\Git\emarlow\6831\GCG_6831_SETTINGS.sql

---
+++ GCG_6831_SETTINGS.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "GCG_6831_SETTINGS" (
+ "REC_ID" INTEGER,
+ "RemoteHost" CHAR(50),
+ "RemoteUser" CHAR(50),
+ "RemotePassword" CHAR(50),
+ "RemotePort" CHAR(50),
+ "RemotePath" CHAR(50));
